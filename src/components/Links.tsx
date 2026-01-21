1f7bd51ce1d3eef3a0f950dc986f864c8322d3c5
Picancianmartin <pietra.cmartin@gmail.com>
2026-01-21 18:23:57 -0300

    adicionando o Cancian. ps: rota de ir para secao contato ok, mas botao voltar para projetos não

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -71,7 +71,7 @@ export default function Links() {
           </div>
 
           <h1 className="text-3xl font-bold tracking-tight text-white drop-shadow-lg">
-            Pietra Martin
+            Pietra Cancian Martin
           </h1>
 
           <p className="text-sm text-gray-300 mt-2 font-medium">
